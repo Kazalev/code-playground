b0e372fa6258710c6a4c5ef8c0d66a9f416fdcec
Kristian Kazalev <kristian.kazalev@abv.bg>
2024-05-15 14:26:56 +0300

    Add dojo-helpdesk app

---
+++ Next.js/dojo-helpdesk/app/(dashboard)/tickets/[id]/page.jsx
@@ -0,0 +1,62 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { notFound } from 'next/navigation'
+import DeleteButton from './DeleteButton'
+
+export const dynamicParams = true
+
+export async function generateMetadata({ params }) {
+    const supabase = createServerComponentClient({ cookies })
+    const { data: ticket, error } = await supabase.from('Tickets').select().eq('id', params.id).single()
+
+    if (error) console.log(error.message)
+
+    return {
+        title: `Dojo Helpdesk | ${ticket?.title || 'Ticket not found'}`
+    }
+}
+
+// export async function generateStaticParams() {
+//     const res = await fetch('http://localhost:4000/tickets')
+//     const tickets = await res.json()
+
+//     return tickets.map(({ id }) => ({ id }))
+// }
+
+const getTicket = async (id) => {
+    // await new Promise((resolve) => setTimeout(resolve, 3000))
+
+    const supabase = createServerComponentClient({ cookies })
+    const { data, error } = await supabase.from('Tickets').select().eq('id', id).single()
+
+    if (!data) {
+        notFound()
+    }
+
+    return data
+}
+
+const TicketDetails = async ({ params }) => {
+    const ticket = await getTicket(params.id)
+
+    const supabase = createServerComponentClient({ cookies })
+    const { data } = await supabase.auth.getSession()
+
+    return (
+        <main>
+            <nav>
+                <h2>Ticket Details</h2>
+                <div className='ml-auto'>{data.session.user.email === ticket.user_email && <DeleteButton id={ticket.id} />}</div>
+            </nav>
+
+            <div className='card'>
+                <h3>{ticket.title}</h3>
+                <small>Created by {ticket.user_email}</small>
+                <p>{ticket.body}</p>
+                <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+            </div>
+        </main>
+    )
+}
+
+export default TicketDetails
